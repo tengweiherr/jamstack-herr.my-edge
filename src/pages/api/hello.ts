e460b0703a01a58a3bdd9b7c44169cb162c170d8
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-20 12:50:49 +0800

    fix edge API

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -5,13 +5,8 @@ export const config = {
   runtime: "edge",
 };
 
-export default async function handler(
-  req: NextRequest
-): Promise<Response> {
-  return new Response(JSON.stringify({ name: "John Doe" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
+const errorResponse = () => new Response("Error message", {
+  status: 400,
+});
+
+export default errorResponse
